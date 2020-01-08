c21185041e2a5c41468e12da3d79835018d863f5
denzelsc1337 <denzelsotomayor@gmail.com>
2020-01-08 00:09:31 -0500

    Mi primer Repositorio

---
+++ src/calculadora/calculadoraTest.java
@@ -0,0 +1,24 @@
+package calculadora;
+
+import org.junit.jupiter.api.Test;
+
+public class calculadoraTest {
+
+	@Test
+	public void testCalculadora() {
+		
+		double resultado = Calculadora.suma(3, 2);
+		int resultado2 = Calculadora.multiplicacion(4, 2);
+		double resultado3 = Calculadora.division(10.2, 2.5);
+		
+
+		
+		System.out.println(resultado);
+		System.out.println(resultado2);
+		System.out.println(resultado3);
+		
+	}
+	
+	
+
+}
